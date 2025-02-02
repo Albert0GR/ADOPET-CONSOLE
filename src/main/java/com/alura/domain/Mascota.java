2d717e68e86f7d9f72d6e017b65473d5dcb3c918
Albert0GR <albertogr0121@gmail.com>
2025-02-02 08:34:32 -0600

    a3-creando dominios

---
+++ src/main/java/com/alura/domain/Mascota.java
@@ -0,0 +1,52 @@
+package com.alura.domain;
+
+public class Mascota {
+
+    public Mascota() {
+    }
+
+    public Mascota(String tipo, String nombre, String raza, int edad, String color, Float peso) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.color = color;
+        this.peso = peso;
+    }
+
+    private Long id;
+    private String tipo;
+    private String nombre;
+    private String raza;
+    private int edad;
+    private String color;
+    private Float peso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+}
---
+++ src/main/java/com/alura/domain/Refugio.java
@@ -0,0 +1,39 @@
+package com.alura.domain;
+
+public class Refugio {
+
+    public Refugio() {}
+
+    //constructor
+    public Refugio(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    private Long id;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private Mascota[] pets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Mascota[] getPets() {
+        return pets;
+    }
+}
